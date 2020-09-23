e9ef9e86a738a588366d1bb2610eb72f4cb3b939
sedddi <63574818+sedddi@users.noreply.github.com>
2020-09-23 17:47:34 +0900

    Add files via upload

---
+++ show.php
@@ -0,0 +1,8 @@
+<?php $id = $_POST['id']; $pw = $_POST['pw']; $name = $_POST['name']; $gender = $_POST['gender'];
+
+echo "로그인 아이디 : $id<br>";
+echo "비밀번호 : $pw <br>";
+echo "이름 : $name <br>";
+echo "성별 : $gender <br>";
+
+?>
